29aeff4e1ad8d5c476f91192cc04ee49f542812f
harpreet-1 <hpsllg8@gmail.com>
2023-08-21 17:44:50 +0530

    done

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const userModel = require("../Models/UserModel");
 const userRouter = express.Router();
-// const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 userRouter.post("/signup", async (req, res) => {
   try {
